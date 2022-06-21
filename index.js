952e34ec1fc84fdd85dc18741a5070d0b3340858
Marcus Vinícius Richa <marcus.richa@protonmail.com>
2022-06-20 23:07:29 -0300

    Finish basic web UI. Add 2 aux math functions

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
 
 
 const add = (a, b) => {
-    let result = a + b;
-    
-    return result;
+	let result = a + b;
+	
+	return result;
 };
 const subtract = (a, b) => {
-    let result = a - b;
-    
-    return result;
+	let result = a - b;
+	
+	return result;
 };
 const multiply = (a, b) => {
-    let result = a * b;
-    
-    return result;
+	let result = a * b;
+	
+	return result;
 };
 const divide = (a, b) => {
-    let result = a / b;
-    
-    return result;
+	let result = a / b;
+	
+	return result;
+};
+const power = (a, b = 2) => {
+	let result = a ** b;
+	
+	return result;
+};
+const rad = (a, b = 0.5) => {
+	let result = a ** b; 
+	
+	return result;
 };
 
 
 function operate(operator, num1, num2) {
-    let result = 0;
-    
-    if (operator === '+') {
-        result = add(num1, num2);
-    } else if (operator === '-') {
-        result = subtract(num1, num2);
-    } else if (operator === '*') {
-        result = multiply(num1, num2);
-    } else if (operator === '/') {
-        result = divide(num1, num2);
-    } else {
-        console.log('Operator not allowed');
-    }
-    
-    console.log(result);
+	let result = 0;
+	
+	if (operator === '+') {
+		result = add(num1, num2);
+	} else if (operator === '-') {
+		result = subtract(num1, num2);
+	} else if (operator === '*') {
+		result = multiply(num1, num2);
+	} else if (operator === '/') {
+		result = divide(num1, num2);
+	} else if (operator === '**') {
+		result = power(num1);
+	} else if (operator === '&#8730') {
+		result = rad(num1);
+	} else {
+		console.log('Operator not allowed');
+	}
+	
+	console.log(result);
 }
 
 
